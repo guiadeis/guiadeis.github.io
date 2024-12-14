053791619b4601ba32d3e0903d564ef714af009c
Jose F Rivas C <josefelixrc7@gmail.com>
2024-12-14 16:22:32 -0400

    feat: Improve script

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
+
 $(function()
 {
-    ResizeIframe('#iframe_content');
+    var iframe_height = 0;    
+    setInterval(ResizeIframe, 1000, '#iframe_content');
 });
 
 const ResizeIframe = function(iframe)
 {
-    let iframe_height = $(iframe).contents().find("html").height();
+    let iframe_height_new = $(iframe).contents().find("html").height();
+    if(iframe_height_new == iframe_height)
+        return;
+
+    iframe_height = iframe_height_new;
+    console.log(iframe_height);
     
-    $(iframe).height(iframe_height);
+    if(iframe_height < 100)
+        $(iframe).height(1080);
+    else
+        $(iframe).height(iframe_height);
 }
