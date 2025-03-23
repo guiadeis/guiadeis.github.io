c395134d837a3c5668d7c9d6ad241a4ae6df6313
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-23 12:19:49 -0400

    feat: Modify script js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
 $(function()
 {
-    const ResizeIframe = function(iframe)
-    {
-        let iframe_height_new = $(iframe).contents().find("html").height();
-        if(iframe_height_new == iframe_height)
-            return;
     
-        iframe_height = iframe_height_new;
-        console.log(iframe_height);
-        
-        if(iframe_height < 100)
-            $(iframe).height(1080);
-        else
-            $(iframe).height(iframe_height);
-    }
-    
-    var iframe_height = 0;
-    $('#iframe_content').on('load', function()
-    {
-        setInterval(ResizeIframe, 1000, '#iframe_content');
-    });
 });
