0b3ac4ef9ebaf6c877cdd150126cf8f3d3579378
Jose F Rivas C <josefelixrc7@gmail.com>
2025-03-29 23:41:36 -0400

    feat: Improve V1

--- js/aside.js
+++ js/aside.js
@@ -1,57 +1,57 @@
 export const aside_elements = [
     {
         id: "inicio"
-        ,link: "/online/index.html"
+        ,link: "../inicio/index.html"
         ,text: "Inicio"
     }
     ,{
         id: "cap1"
-        ,link: "/online/cap1/index.html"
+        ,link: "../cap1/index.html"
         ,text: "1. Introducci&oacute;n a la Ingenier&iacute;a de Sistemas"
     }
     ,{
         id: "cap2"
-        ,link: "/online/cap2/index.html"
+        ,link: "../cap2/index.html"
         ,text: "2. Ciencia de Sistemas y Pensamiento Sist&eacute;mico"
     }
     ,{
         id: "cap3"
-        ,link: "/online/cap3/index.html"
+        ,link: "../cap3/index.html"
         ,text: "3. Ciclo de Vida de Sistemas"
     }
     ,{
         id: "cap4"
-        ,link: "/online/cap4/index.html"
+        ,link: "../cap4/index.html"
         ,text: "4. Modelo ISO 15288"
     }
     ,{
         id: "cap5"
-        ,link: "/online/cap5/index.html"
+        ,link: "../cap5/index.html"
         ,text: "5. Representando Sistemas con Modelos"
     }
     ,{
         id: "cap6"
-        ,link: "/online/cap6/index.html"
+        ,link: "../cap6/index.html"
         ,text: "6. Campos y Dominio de la Ingenier&iacute;a de Sistemas"
     }
     ,{
         id: "cap7"
-        ,link: "/online/cap7/index.html"
+        ,link: "../cap7/index.html"
         ,text: "7. Tendencias, Desaf&iacute;os y Futuro"
     }
     ,{
         id: "cap8"
-        ,link: "/online/cap8/index.html"
+        ,link: "../cap8/index.html"
         ,text: "8. Recursos Adicionales"
     }
     ,{
         id: "cap9"
-        ,link: "/online/cap9/index.html"
+        ,link: "../cap9/index.html"
         ,text: "9. Ejemplos de Implementaci&oacute;n"
     }
     ,{
         id: "bibliografia"
-        ,link: "/online/bibliografia/index.html"
+        ,link: "../bibliografia/index.html"
         ,text: "Bibliograf&iacute;a"
     }
 ];
